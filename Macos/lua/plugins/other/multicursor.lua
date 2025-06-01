e5905b0d54db984b08330263161fbf4ae934eec5
ivyhole <792336362@qq.com>
2025-06-01 21:37:52 +0800

    fix:Update multicursor

--- Macos/lua/plugins/other/multicursor.lua
+++ Macos/lua/plugins/other/multicursor.lua
@@ -1,27 +1,27 @@
-return{
-    {
+return {
+  {
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
     event = "BufReadPost",
-    -- keys = {
-    --   -- Append/insert for each line of visual selections. Similar to block selection insertion.
-    --   {
-    --     "mI",
-    --     function()
-    --       require("multicursor-nvim").insertVisual()
-    --     end,
-    --     mode = "x",
-    --     desc = "Insert cursors at visual selection",
-    --   },
-    --   {
-    --     "mA",
-    --     function()
-    --       require("multicursor-nvim").appendVisual()
-    --     end,
-    --     mode = "x",
-    --     desc = "Append cursors at visual selection",
-    --   },
-    -- },
+    keys = {
+      -- Append/insert for each line of visual selections. Similar to block selection insertion.
+      {
+        "mI",
+        function()
+          require("multicursor-nvim").insertVisual()
+        end,
+        mode = "x",
+        desc = "Insert cursors at visual selection",
+      },
+      {
+        "mA",
+        function()
+          require("multicursor-nvim").appendVisual()
+        end,
+        mode = "x",
+        desc = "Append cursors at visual selection",
+      },
+    },
     config = function()
       local mc = require("multicursor-nvim")
       mc.setup()
@@ -36,3 +36,4 @@ return{
     end,
   },
 }
+
